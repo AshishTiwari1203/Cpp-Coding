62586e5aea30e96ce49d7b167025819375759f3e
Ashish <ashishktiwari1203@gmail.com>
2023-05-17 21:03:40 +0530

    push operation performed

--- Queue/queue1.cpp
+++ Queue/queue1.cpp
@@ -1,8 +1,19 @@
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    queue<int> q;
+
+    q.push(12);
+    q.push(13);
+    q.push(14);
+
+    cout << "Size of q: " << q.size() << endl;
+    cout << "Empty or not: " << q.empty() << endl;
+    cout << "Front element: " << q.front() << endl;
+    cout << "End element: " << q.back() << endl;
 
     return 0;
 }
